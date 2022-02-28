65cb3ff98fd557b54adb9397a36a9a89c9f2b8d1
Eric Woolard <sdamien6@gmail.com>
2022-02-27 21:43:45 -0600

    Small tweaks to InfoSex

--- src/App.js
+++ src/App.js
@@ -49,16 +49,7 @@ function App() {
             align="right"
             img="hayfield.png"
             imgAlt="Image of a concert"
-            title="venue"
-            buttonText="hello">
-              <p>MS Horse Park is partnered with the City of Starkville, Oktibbeha County, and MSU to provide their guests with the best possible entertainment experience.<br/><br/>This impressive facility boasts a massive 100 acre plot of land at which it hosts over 40,000 people every year. As an extension of MSU, it’s located just south of campus on Poor House Rd.<br/><br/>We are thrilled to be partnering with the City of Starkville and MSU to officially bring back the 2nd annual Hayday Fest to Starkville Ms!<br/><br/> See you on the Hayfield! 
-            </p>
-          </InfoSex>
-          <InfoSex 
-            align="left"
-            img="hayfield.png"
-            imgAlt="Image of a concert"
-            title="venue">
+            title="faq">
               <p>MS Horse Park is partnered with the City of Starkville, Oktibbeha County, and MSU to provide their guests with the best possible entertainment experience.<br/><br/>This impressive facility boasts a massive 100 acre plot of land at which it hosts over 40,000 people every year. As an extension of MSU, it’s located just south of campus on Poor House Rd.<br/><br/>We are thrilled to be partnering with the City of Starkville and MSU to officially bring back the 2nd annual Hayday Fest to Starkville Ms!<br/><br/> See you on the Hayfield! 
             </p>
           </InfoSex>
